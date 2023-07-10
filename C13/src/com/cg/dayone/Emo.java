1bcb73fb9c3914ef2720bf210c0a3a181dae80c9
madha <madha@LAPTOP-4S67TGIJ>
2023-07-10 09:43:52 +0530

    First Commit

---
+++ C13/src/com/cg/dayone/Emo.java
@@ -0,0 +1,25 @@
+package com.cg.dayone;
+
+public class Emo {
+
+	public static void main(String[] args) {
+		
+			System.out.println("\uD83D\uDE0A"); // 
+	        System.out.println("\uD83D\uDE02"); 	       
+	        System.out.println("\uD83D\uDE0D"); 
+	        System.out.println("\uD83D\uDE09"); 	       
+	        System.out.println("\uD83D\uDE18"); 
+	        System.out.println("\uD83D\uDE3B"); 
+	        System.out.println("\uD83D\uDC36"); 
+	        System.out.println("\uD83D\uDE21"); 
+	        System.out.println("\uD83D\uDC80"); 
+	        System.out.println("\uD83D\uDC4F"); 
+	        System.out.println("\uD83D\uDC4D"); 
+	        System.out.println("\uD83D\uDC4E"); 
+	        System.out.println("\uD83C\uDF89"); 
+	        System.out.println("\uD83C\uDF82"); 
+	        System.out.println("\uD83C\uDF84");
+
+	}
+
+}
